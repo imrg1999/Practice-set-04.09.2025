97f65babba4cbbbf53d9bf510212d6d6e35bb73c
rupankar1999 <rupankar.ghosh1999@gmail.com>
2025-09-04 12:48:50 +0530

    authRoutes added and connected to server

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import userRoutes from './Routes/userRoutes.js'
+import { connSetup } from './Config/mongooseConnect.js';
+import authRoutes from './Routes/authRoutes.js'
 
 const app = express();
 const port = 3000;
@@ -8,6 +10,9 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
 app.use('/api',userRoutes);
+app.use('/auth',authRoutes);
+
+connSetup();
 
 app.get('/', (req,res) => {
     res.status(200).json({
